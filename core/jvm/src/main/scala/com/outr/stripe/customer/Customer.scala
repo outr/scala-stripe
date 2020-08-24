04d9242b654fba0186ff24dad28a56299e68334e
Naden <naden@harana.com>
2020-08-24 12:50:43 +1000

    Updated Customer to reflect latest attributes

--- core/jvm/src/main/scala/com/outr/stripe/customer/Customer.scala
+++ core/jvm/src/main/scala/com/outr/stripe/customer/Customer.scala
@@ -1,12 +1,14 @@
 package com.outr.stripe.customer
 
-import com.outr.stripe.charge.{Card, Shipping}
+import com.outr.stripe.charge.{Address, Card, Shipping}
 import com.outr.stripe.subscription.Subscription
 import com.outr.stripe.{Money, StripeList}
 
 case class Customer(id: String,
                     `object`: String,
                     accountBalance: Money,
+                    address: Option[Address],
+                    balance: Option[Int],
                     created: Long,
                     currency: Option[String],
                     defaultSource: Option[String],
@@ -14,8 +16,13 @@ case class Customer(id: String,
                     description: Option[String],
                     discount: Option[Discount],
                     email: Option[String],
+                    invoicePrefix: Option[String],
                     livemode: Boolean,
                     metadata: Map[String, String],
+                    name: Option[String],
+                    nextInvoiceSequence: Option[Int],
+                    phone: Option[String],
                     shipping: Option[Shipping],
                     sources: StripeList[Card],
-                    subscriptions: StripeList[Subscription])
+                    subscriptions: StripeList[Subscription],
+                    taxExempt: Option[String])
